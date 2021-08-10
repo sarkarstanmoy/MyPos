dca31e56911be6b8f688fc20c6dfbc2436c89727
Tanmoy Sarkar <Tanmoy.Sarkar@neudesic.com>
2021-08-10 09:45:35 +0530

    Add login functionality

---
+++ src/WebAPI/MyPos/Identity.API/Controllers/AccountController.cs
@@ -0,0 +1,38 @@
+﻿using Identity.API.Infrastructure.Repositories;
+using Identity.API.Model;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using static Identity.API.Command.PostCommand;
+using static Identity.API.Queries.GetValuesQuery;
+
+namespace Identity.API.Controllers
+{
+    [Route("api/v1/[controller]")]
+    public class AccountController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public AccountController(IMediator mediator)
+        {
+            _mediator = mediator ?? throw new ArgumentNullException(nameof(mediator));
+        }
+
+        [HttpPost]
+        [Route("login")]
+        public async Task<IActionResult> Login([FromBody] User user)
+        {
+            return Ok(await _mediator.Send(new GetUserQuery(user)));
+        }
+
+        [HttpPost]
+        [Route("register")]
+        public async Task<IActionResult> Register([FromBody] User user)
+        {
+            return Ok(await _mediator.Send(new PostUserCommand(user)));
+        }
+    }
+}
---
+++ src/WebAPI/MyPos/Identity.API/Infrastructure/IdentityContext.cs
@@ -0,0 +1,23 @@
+﻿using Identity.API.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace Identity.API.Infrastructure
+{
+    public class IdentityContext : DbContext
+    {
+        public DbSet<Credentials> Credentials { get; set; }
+
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Credentials>(entity =>
+            {
+                entity.ToTable("credentials");
+                entity.HasKey(e => e.UserName);
+            });
+        }
+    }
+}
